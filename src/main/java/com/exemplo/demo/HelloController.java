eb048aee1d2921ecf03701c19e3dfe6174526b5f
Anderson Luiz Sartori <anderson-sart@hotmail.com>
2025-11-26 20:44:37 -0300

    Adicionando projeto Spring Boot

---
+++ src/main/java/com/exemplo/demo/HelloController.java
@@ -0,0 +1,13 @@
+package com.exemplo.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Spring Boot funcionando com sucesso!";
+    }
+}
